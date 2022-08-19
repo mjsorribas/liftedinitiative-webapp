8bc582cd7a0f8168ea02756a0d74ab934481f338
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-08-18 21:17:56 -0300

    Add Logo compoent

---
+++ src/lib/components/logo/Logo.tsx
@@ -0,0 +1,20 @@
+import { Image, Box, Text } from '@chakra-ui/react';
+
+const LOGOSVG = (props: any) => {
+    return (
+            <Box display='flex' alignItems='center'>
+                <Image
+                    boxSize='100px'
+                    objectFit='cover'
+                    src="/assets/lifted.svg"
+                    title="The Lifted initiative"
+                />
+                <Text fontSize={'sm'}>
+                THELIFTEDINITIATIVE
+                </Text>
+            </Box>
+    );
+  };
+  export default function Logo() {
+    return (<LOGOSVG />)
+  }
